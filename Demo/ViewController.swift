6f598090b935ed76b8d93822c7b0700530436040
peterpan <apppeterpan@gmail.com>
2020-12-10 15:47:30 +0800

    Initial Commit

---
+++ Demo/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by SHIH-YING PAN on 2020/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        updateZoomSizeFor(size: view.bounds.size)
+    }
+
+    func updateZoomSizeFor(size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = scale
+        scrollView.maximumZoomScale = max(widthScale, heightScale)
+        scrollView.zoomScale = scale
+    }
+    
+}
+
+extension ViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let inset = (scrollView.bounds.height - imageView.frame.height) / 2
+        scrollView.contentInset = .init(top: max(inset, 0), left: 0, bottom: 0, right: 0)
+    }
+}
